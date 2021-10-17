05113da9c1baccc158a2392e161659840106e610
javier gomez prida <72632242+javierprida@users.noreply.github.com>
2021-10-17 16:41:23 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function() {
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+
+    });
+
+    // smooth scrolling 
+
+    $('a[href*="#"]').on('click', function(e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+                scrollTop: $($(this).attr('href')).offset().top,
+
+            },
+            500,
+            'linear'
+        );
+
+    });
+
+});
+
+//download cv
+function download(filename, textInput) {
+
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput));
+    element.setAttribute('download', filename);
+    document.body.appendChild(element);
+    element.click();
+    //document.body.removeChild(element);
+}
+document.getElementById("btn")
+document.addEventListener("click", function() {
+    let text = document.getElementById("text").value;
+    let filename = "output.txt";
+    download(filename, text);
+}, false);
+
+//modal
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) => {
+    const openBtn = document.getElementById(openButton),
+        modalContainer = document.getElementById(modalContent)
+
+    if (openBtn && modalContainer) {
+        openBtn.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+showModal('open-modal', 'modal-container')
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal() {
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
